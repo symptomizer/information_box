eb4bad6ec927132644ff28d0d85ab993a9e221f2
Mohammad Javad <jkhazali@gmail.com>
2021-03-30 22:33:19 +0100

    proper error handling and checking for alias and whatnot

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from fuzzywuzzy import process
 from flask_cors import CORS
 import wptools
 import re
+import json
 
 cleanr = re.compile('\[[^\]]*\||\[\[|\]\]')
 # removing_pipes = re.compile('\[[^\]]*\||')
@@ -37,26 +38,36 @@ def process_info_box(infobox):
                 returned_infobox[key] = clean_text 
     return returned_infobox
 
+def responsify(data, status, error):
+    return {'data': data, 'status': status, 'error': error}
+
 @app.route('/')
 def index():
     return "Hello, World!"
 
 @app.route('/info')
 def info():
-    title = request.args.get('search')
-    page = wptools.page(title)
-    page.get_query()
-    return_obj = {}
-    images = page.images(['url'])
-    extext = page.data['extext']
-    aliases = page.data['aliases']
-    return_obj['images'] = [img['url'] for img in images]
-    return_obj['extext'] = extext
-    return_obj['aliases'] = aliases
-    wikidata = wptools.page(title).get_parse()
-    infobox = wikidata.data['infobox']
-    return_obj['infobox'] = process_info_box(infobox)
-    return jsonify(return_obj)
-
+    try:
+        title = request.args.get('search')
+        page = wptools.page(title)
+        page.get_query()
+        return_obj = {}
+        extext = ""
+        aliases = []
+        images = page.images(['url'])
+        if 'extext' in page.data:
+            extext = page.data['extext']
+        if 'aliases' in page.data:
+            aliases = page.data['aliases']
+        return_obj['images'] = [img['url'] for img in images]
+        return_obj['extext'] = extext
+        return_obj['aliases'] = aliases
+        wikidata = wptools.page(title).get_parse()
+        infobox = wikidata.data['infobox']
+        return_obj['infobox'] = process_info_box(infobox)
+        return jsonify(responsify(return_obj, 200, ""))
+    except Exception as e:
+        print("Incorrect")
+        return jsonify(responsify({}, 400, str(e)))
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8889)
